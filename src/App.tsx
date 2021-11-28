f45ac4cd1bb881b8e0a1a0c7b1c0b6df41e5c169
Lucas Assis <lucasassis@lojadomecanico.com.br>
2021-11-28 11:29:20 -0300

    main - estrutura inicial

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from './routes';
+import { Header } from './pages/Header/index';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Repository: React.FC = () => {
+
+    return (
+        <h1>Página de repositórios</h1>
+    )
+}
